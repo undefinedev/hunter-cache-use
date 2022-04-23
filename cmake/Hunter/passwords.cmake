4544bbde188892b01766d9bd68bf267a88d94939
undefinedev <undefined_dev@inbox.ru>
2022-04-23 19:47:37 +0300

    first probe

---
+++ cmake/Hunter/passwords.cmake
@@ -0,0 +1,11 @@
+hunter_upload_password(
+        # REPO_OWNER + REPO = https://github.com/undefinedev/hunter-cache
+        REPO_OWNER "udnefinedev"
+        REPO "hunter-cache"
+
+        # USERNAME = https://github.com/defineddev
+        USERNAME "defineddev"
+
+        # PASSWORD = GitHub token saved as a secure environment variable
+        PASSWORD "$ENV{GITHUB_USER_PASSWORD}"
+)
